3d7fe8cd59027988141d15360f64d6db89332a6a
oscar6echo <olivier.borderies@gmail.com>
2020-05-19 08:31:38 +0200

    1af30c0

---
+++ README.md
@@ -0,0 +1,49 @@
+# JOB Multi ID Provider Auth Page
+
+See [documentation](https://observablehq.com/@oscar6echo/jupyter-observablehq-bridge).
+
+## Definition
+
+A demo instance of a **JOB Auth** web page (JOBA) is available at https://jupyter-observablehq-bridge.github.io/multi-id-auth-page.
+
+It allows a user to authenticate with a number of ID providers:
+
+- [Google single signin](https://developers.google.com/identity/sign-in/web/sign-in)
+- SG Connect (corporate example)
+
+To add/remove other ID providers simply clone the repo and publish your own JOBA.
+
+Most main stream ID providers offer off-the-shelf JavaScript libraries that encapsulate the complexity of the OAuth 2.0 mechanism (redirection, refresh, etc), are typically imported via top level `<script>` tags, and then simple to use.
+
+Consequently a JOBA can be a simple aggregate of several ID providers user friendly authentication libraries.  
+To make that even simpler the demo JOBA is written in [Vue](https://vuejs.org/) for structure but does **not** require a build step.  
+So it is easy to fork and adapt.
+
+## Dev
+
+- launch web servers:
+
+```bash
+python -m http.server 3000
+python -m http.server 3001
+```
+
+- Open http://localhost:3001/opener.html
+- Click button "Open Auth Page..." and browser console
+- This will open page http://localhost:3000
+- Login to Google and/or SG Connect
+- See http://localhost:3001/opener.html console
+
+## Deploy
+
+- Install [push-dir](https://www.npmjs.com/package/push-dir)
+
+```bash
+yarn install
+```
+
+- push `master` branch to `gh-pages`.
+
+```bash
+yarn deploy
+```
---
+++ components/app.vue
@@ -0,0 +1,146 @@
+<template>
+  <div>
+    <v-app-bar app color="blue accent-3" dark>
+      <v-spacer></v-spacer>
+      <v-toolbar-title>Multi ID Provider Auth Page</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <a href="https://github.com/jupyter-observablehq-bridge/multi-id-auth-page" target="_blank">
+        <v-icon>mdi-github-circle</v-icon>
+      </a>
+    </v-app-bar>
+
+    <v-content>
+      <v-container fluid>
+        <v-row class="d-flex justify-center mt-5 mb-2">
+          <div
+            class="text-center font-italic font-weight-normal"
+          >Do not close this page for the parent window to get updated when tokens refresh</div>
+        </v-row>
+
+        <v-row class="d-flex justify-center mt-2 mb-5">
+          <table>
+            <tr>
+              <td class="text-left font-weight-light tleft">Parent Window</td>
+              <td class="text-left font-weight-bold tright">{{parentWin}}</td>
+            </tr>
+            <tr>
+              <td class="text-left font-weight-light tleft">Last Update Sent</td>
+              <td id="time" class="text-left font-weight-bold tright">{{lastMsgTime}}</td>
+            </tr>
+          </table>
+        </v-row>
+
+        <div>
+          <v-row no-gutters>
+            <v-col cols="1"></v-col>
+            <v-col cols="5">
+              <google class="mr-1" :scope-in="scope.google" @update="updateCreds" />
+            </v-col>
+            <v-col cols="5">
+              <sgconnect :scope-in="scope.sgconnect" @update="updateCreds" />>
+            </v-col>
+          </v-row>
+        </div>
+      </v-container>
+    </v-content>
+  </div>
+</template>
+
+<script>
+const Google = httpVueLoader("./google.vue");
+const Sgconnect = httpVueLoader("./sgconnect.vue");
+
+module.exports = {
+  name: "App",
+  components: {
+    Google,
+    Sgconnect
+  },
+  data() {
+    return {
+      parentWin: "None",
+      params: {},
+      creds: {},
+      lastMsgTime: null,
+      scope: {}
+    };
+  },
+  computed: {},
+  mounted() {
+    this.readParams();
+    if (window.opener) this.setListener();
+  },
+  methods: {
+    setListener() {
+      window.opener.postMessage("url", "*");
+
+      const listener = window.addEventListener("message", evt => {
+        // console.log(evt);
+        const { protocol, host } = window.location;
+        if (event.origin === protocol + "//" + host) return;
+        // console.log("candidate event");
+        // console.log(evt.data);
+        if (evt.data && evt.data.url) this.parentWin = evt.data.url;
+        removeEventListener("message", listener);
+      });
+    },
+
+    updateCreds(data) {
+      console.log("-- updateCreds");
+      //   console.log(data);
+      this.creds[data.provider] = data.creds;
+      this.sendCreds();
+    },
+
+    sendCreds() {
+      console.log("-- sendCreds");
+      console.log(this.creds);
+      this.lastMsgTime = new Date().toLocaleTimeString("fr-FR");
+      if (!window.opener){
+          console.log('******** WARNING');
+          console.log('THIS PAGE MUST BE OPENED FROM ANOTHER ONE');
+          console.log('SEE DOCUMENTATION https://observablehq.com/@oscar6echo/jupyter-observablehq-bridge');
+          return
+      }
+      window.opener.postMessage({ updateCreds: this.creds }, "*");
+    },
+
+    readParams() {
+      const urlParams = new URLSearchParams(window.location.search);
+      this.scope = {
+        google: urlParams.get("google-scope"),
+        sgconnect: urlParams.get("sgconnect-scope")
+      };
+    }
+  }
+};
+</script>
+
+<style scoped>
+.table {
+  table-layout: fixed;
+}
+
+.tleft {
+  width: 150px;
+}
+.tright {
+  width: 200px;
+  max-width: 550px;
+  word-wrap: break-word;
+}
+</style>
+
+<style >
+.my-wrapper {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  border: 1px solid lightgrey;
+  height: 750px;
+}
+
+.wrapper-btn {
+  height: 200px;
+}
+</style>
